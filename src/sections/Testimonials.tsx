d84d2a38c34fe9fd750f2aab106665f8e83166c4
Giggs471 <blitzwizard45@gmail.com>
2024-08-11 22:00:10 +0500

    commited all

---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,59 @@
+import avatar1 from "@/assets/avatar-1.png";
+import avatar2 from "@/assets/avatar-2.png";
+import avatar3 from "@/assets/avatar-3.png";
+import avatar4 from "@/assets/avatar-4.png";
+import Image from "next/image";
+
+const testimonials = [
+  {
+    text: "“This product has completely transformed how I manage my projects and deadlines”",
+    name: "Sophia Perez",
+    title: "Director @ Quantum",
+    avatarImg: avatar1,
+  },
+  {
+    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight”",
+    name: "Jamie Lee",
+    title: "Founder @ Pulse",
+    avatarImg: avatar2,
+  },
+  {
+    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours”",
+    name: "Alisa Hester",
+    title: "Product @ Innovate",
+    avatarImg: avatar3,
+  },
+  {
+    text: "“Our team's productivity has increased significantly since we started using this tool”",
+    name: "Alec Whitten",
+    title: "CTO @ Tech Solutions",
+    avatarImg: avatar4,
+  },
+];
+
+export const Testimonials = () => {
+  return <section className="py-20 md:py-24">
+    <div className="container">
+      <h2 className="text-5xl text-center tracking-tighter font-medium md:text-6xl">Beyond Expectations</h2>
+      <p className="text-white/70 text-lg text-center mt-5 tracking-tight max-w-sm mx-auto md:text-xl">Our revolutionary AI SEO tools have transformed our client's strategies</p>
+      <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <div className="flex gap-5">
+          {testimonials.map((testimonial) =>(
+            <div className="md:p-10 border border-white/15 p-6 rounded-xl md:max-w-md bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs flex-none" key={testimonial.name}>
+              <div className="md:text-2xl text-lg tracking-tight">{testimonial.text} </div>
+              <div className="flex items-center gap-3 mt-5">
+                <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                  <Image src={testimonial.avatarImg} alt={`Avatar for ${testimonial.name}`} className="h-11 w-11 rounded-lg grayscale "/>
+                </div>  
+                <div className="">
+                  <div className="">{testimonial.name}</div>
+                  <div className="text-white/50 text-sm">{testimonial.title}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  </section>;
+};
